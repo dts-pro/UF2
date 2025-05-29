68fca107dcee7a827f98de3e50ecb9d99c60b7a9
David Tur <d.tursanmateu@edu.gva.es>
2025-05-29 12:53:54 +0200

    Remove obsolete HTML documentation and associated images from the project, including the 'objectius.html' file and various PNG images related to sections 3 and 4. Additionally, delete the 'vp-icons.css' file as it is no longer needed.

--- docs/assets/chunks/stateDiagram-v2-YXO3MK2T.DD6kVijJ.js
+++
@@ -1,1 +0,0 @@
-import{s as e,b as r,a,S as s}from"./chunk-AEK57VVT.CrnL0dn0.js";import{_ as i}from"../app.BXc7cd5k.js";import"./chunk-RZ5BOZE2.C5-e9vyh.js";import"./framework.D8ILtLtz.js";import"./theme.CrzTF4LO.js";var p={parser:a,get db(){return new s(2)},renderer:r,styles:e,init:i(t=>{t.state||(t.state={}),t.state.arrowMarkerAbsolute=t.arrowMarkerAbsolute},"init")};export{p as diagram};
