e6a7eb973617fc8484b2a2d4d039545ad498fd8e
David Tur <d.tursanmateu@edu.gva.es>
2025-05-29 13:19:04 +0200

    Remove obsolete HTML documentation and associated image files from the project.

--- docs/assets/chunks/channel.BXcJEBez.js
+++
@@ -1,1 +0,0 @@
-import{ah as o,ai as n}from"../app.BXc7cd5k.js";const t=(a,r)=>o.lang.round(n.parse(a)[r]);export{t as c};
--- docs/assets/objectius.md.IjY_13MA.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as a,c as i,o as r,a4 as s}from"./chunks/framework.D8ILtLtz.js";const p=JSON.parse('{"title":"Objectius","description":"","frontmatter":{},"headers":[],"relativePath":"objectius.md","filePath":"objectius.md"}'),t={name:"objectius.md"};function l(n,e,o,c,u,d){return r(),i("div",null,e[0]||(e[0]=[s("",4)]))}const f=a(t,[["render",l]]);export{p as __pageData,f as default};
